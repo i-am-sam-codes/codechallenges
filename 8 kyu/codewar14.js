2f0e7f56431957731d8e574acd53e845b51a86e2
i-am-sam-codes <sammmceja@gmail.com>
2022-02-19 18:20:54 -0800

    Reduce but Grow

---
+++ 8 kyu/codewar14.js
@@ -0,0 +1,9 @@
+//Given a non-empty array of integers, return the result of multiplying the values together in order. 
+function grow(x){
+    let sum=1;
+    for(let i=0;i<x.length;i++)
+    {
+      sum=sum*x[i];
+      }
+    return sum;
+}
