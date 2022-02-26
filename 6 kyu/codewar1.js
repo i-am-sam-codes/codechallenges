71ab5fa0344a002bc6f6e1fc1ee3e0101e4989ac
i-am-sam-codes <sammmceja@gmail.com>
2022-02-26 14:06:40 -0800

    Find the odd int

---
+++ 6 kyu/codewar1.js
@@ -0,0 +1,6 @@
+//Given an array of integers, find the one that appears an odd number of times.
+
+//There will always be only one integer that appears an odd number of times.
+function findOdd(x) {
+    return x.reduce((a, b) => a ^ b);
+}
