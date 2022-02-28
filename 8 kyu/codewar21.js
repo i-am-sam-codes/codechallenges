bdfe13f2add50e0d85a5adf496769282b83556dd
i-am-sam-codes <sammmceja@gmail.com>
2022-02-28 10:32:41 -0800

    Basic Mathematical Operations

---
+++ 8 kyu/codewar21.js
@@ -0,0 +1,13 @@
+//create a function that does four basic mathematical operations
+function basicOp(o, v1, v2)
+{
+  if (o === '+') {
+    return v1 + v2
+  }if (o === '-'){
+    return v1 - v2
+  }if (o === '*') {
+    return v1 * v2
+  }else if ( o === '/') {
+    return v1 / v2
+  }
+}
