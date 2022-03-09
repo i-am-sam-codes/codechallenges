f2af1a394979cbac8ab1ee0a46a5cce861499d46
i-am-sam-codes <sammmceja@gmail.com>
2022-03-08 19:44:56 -0800

    Keep Hydrated!

--- 8 kyu/codewar29.js
+++ 8 kyu/codewar29.js
@@ -1,6 +1,5 @@
 //You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
 
-
 function litres(time) {
     const liters = .5
     const result = time * liters
