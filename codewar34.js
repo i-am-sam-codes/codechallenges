569ad6e1c0be3672d3d5e4fa433880447bda8da8
i-am-sam-codes <sammmceja@gmail.com>
2022-03-22 23:31:05 -0700

    Abbreviate a Two Word Name

--- codewar34.js
+++
@@ -1,8 +0,0 @@
-//Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
-function abbrevName(name){
-    const word = name.split(' ')
-    const result = word.map((e) => e[0]).join('')
-    const sum = result.split('')
-    const dot = sum.splice(1, 0, ".")
-    return sum.join('').toUpperCase()
-}
