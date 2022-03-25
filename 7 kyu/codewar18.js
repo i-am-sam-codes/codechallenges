f28fc553875bb12c71140e94849c0e9a1173b3ad
i-am-sam-codes <sammmceja@gmail.com>
2022-03-25 11:35:49 -0700

    Filter Long Words

--- 7 kyu/codewar18.js
+++ 7 kyu/codewar18.js
@@ -1,6 +1,5 @@
 //Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
 function filterLongWords(sentence, n) {
-    const arr = []
     const word = sentence.split(' ')
     const result = word.filter((e) => e.length > n )
     
