c3ed683ebe38b45c5aaa67cb67bae0c295ef4efc
i-am-sam-codes <sammmceja@gmail.com>
2022-05-13 22:45:01 -0700

    What is between?

--- 8 kyu/codewar51.js
+++ 8 kyu/codewar51.js
@@ -1,8 +1,9 @@
-//Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+//Complete the function that takes two integers (a, b, where a < b) 
+//and return an array of all integers between the input parameters, including them.
 function between(a, b) {
     let arr = []
     for(let i=a; i <= b; i++){
       arr.push(i)
     }
     return arr
-}
+ }
